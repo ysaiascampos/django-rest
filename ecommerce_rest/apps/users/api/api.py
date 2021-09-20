23052098f95be2b9a106e5819429f3c635d48bed
ysaiascampos <ysaiascampos@gmail.com>
2021-09-19 23:08:48 -0300

    Update api.py

--- ecommerce_rest/apps/users/api/api.py
+++ ecommerce_rest/apps/users/api/api.py
@@ -8,10 +8,13 @@ from apps.users.api.serializers import UserSerializer
 @api_view(['GET','POST'])
 def user_api_view(request):
 
+    #list
     if request.method == 'GET':
         users = User.objects.all()
         users_serializer = UserSerializer(users,many = True)
         return Response(users_serializer.data,status = status.HTTP_200_OK)
+
+    #create
     elif request.method == 'POST':
         users_serializer = UserSerializer(data = request.data)
         if users_serializer.is_valid():
@@ -21,17 +24,25 @@ def user_api_view(request):
 
 @api_view(['GET','PUT','DELETE'])
 def user_detail_api_view(request,pk=None):
+    #queryset
     user = User.objects.filter(id = pk).first()
+    
+    #validation
     if user:
+        #retrieve
         if request.method == 'GET':
             user_serializer = UserSerializer(user)
             return Response(user_serializer.data,status = status.HTTP_200_OK)
+        
+        #update
         elif request.method == 'PUT':
             users_serializer = UserSerializer(user,data = request.data)
             if users_serializer.is_valid():
                 users_serializer.save()
                 return Response(users_serializer.data,status = status.HTTP_200_OK)
             return Response(users_serializer.errors,status = status.HTTP_400_BAD_REQUEST)
+
+        #delete
         elif request.method == 'DELETE':
             user.delete()
             return Response({'message':'Usuario Eliminado correctamente!'},status = status.HTTP_200_OK)
