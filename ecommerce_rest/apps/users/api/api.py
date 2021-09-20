b89729354b8fe7543ecb4f47f80c2904edd09f8c
ysaiascampos <ysaiascampos@gmail.com>
2021-09-19 22:49:45 -0300

    con funciones crud

--- ecommerce_rest/apps/users/api/api.py
+++ ecommerce_rest/apps/users/api/api.py
@@ -1,10 +1,38 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from apps.users.models import User
 from apps.users.api.serializers import UserSerializer
 
-class UserAPIView(APIView):
-    def get(self,request):
+@api_view(['GET','POST'])
+def user_api_view(request):
+
+    if request.method == 'GET':
         users = User.objects.all()
         users_serializer = UserSerializer(users,many = True)
         return Response(users_serializer.data)
+    elif request.method == 'POST':
+        users_serializer = UserSerializer(data = request.data)
+        if users_serializer.is_valid():
+            users_serializer.save()
+            return Response(users_serializer.data)
+        return Response(users_serializer.errors)
+
+@api_view(['GET','PUT','DELETE'])
+def user_detail_api_view(request,pk=None):
+
+    if request.method == 'GET':
+        user = User.objects.filter(id = pk).first()
+        user_serializer = UserSerializer(user)
+        return Response(user_serializer.data)
+    elif request.method == 'PUT':
+        user = User.objects.filter(id = pk).first()
+        users_serializer = UserSerializer(user,data = request.data)
+        if users_serializer.is_valid():
+            users_serializer.save()
+            return Response(users_serializer.data)
+        return Response(users_serializer.errors)
+    elif request.method == 'DELETE':
+        user = User.objects.filter(id = pk).first()
+        user.delete()
+        return Response('Eliminado')
--- ecommerce_rest/apps/users/api/urls.py
+++ ecommerce_rest/apps/users/api/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from django.urls import path
-from apps.users.api.api import UserAPIView
+from apps.users.api.api import user_api_view,user_detail_api_view
 
 urlpatterns = [
-    path('usuario/',UserAPIView.as_view(), name = 'usuario_api')
+    path('usuario/',user_api_view, name = 'usuario_api'),
+    path('usuario/<int:pk>',user_detail_api_view,name='usuario_detail_api_view')
 ]
