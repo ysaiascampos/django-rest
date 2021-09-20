6818f4ee869fc72cff0a98e51a64029fb268b79f
ysaiascampos <ysaiascampos@gmail.com>
2021-09-19 22:06:00 -0300

    api usuario

---
+++ ecommerce_rest/apps/users/api/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from django.urls import path
+from apps.users.api.api import UserAPIView
+
+urlpatterns = [
+    path('usuario/',UserAPIView.as_view(), name = 'usuario_api')
+]
